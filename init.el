1028bdcab960abe9526b6e2b4d06571bec998874
dalelyunas <alelyunas.d@husky.neu.edu>
2017-11-16 01:21:18 -0500

    support for javascript and better helm

--- init.el
+++ init.el
@@ -3,6 +3,8 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
+(show-paren-mode 1)
+(setq ring-bell-function 'ignore)
 
 ;;----------------
 ;; LOAD PACKAGES
@@ -15,7 +17,9 @@
 
 ; list the packages you want
 (setq package-list
-    '(async helm gruvbox-theme))
+      '(async helm gruvbox-theme company smartparens js2-mode js2-refactor
+	      xref-js2 company-tern flycheck json-mode tern
+	      magit exec-path-from-shell))
 
 ; activate all the packages
 (package-initialize)
@@ -29,32 +33,128 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-
 ;;----------------
 ;; SETUP PACKAGES
 ;;----------------
 
-;; async
+;; PATH
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;; ASYNC
 (require 'async)
 (async-bytecomp-package-mode 1)
 
-;; helm
+;; JS2-MODE
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; Better imenu
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+(require 'js2-refactor)
+(require 'xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+(define-key js-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+(setq js-indent-level 2)
+
+;; Turn off js2 mode errors & warnings (we lean on eslint/standard)
+(setq js2-mode-show-parse-errors nil)
+(setq js2-mode-show-strict-warnings nil)
+
+;; SMARTPARENS
+(require 'smartparens)
+(smartparens-global-mode 1)
+
+;; COMPANY
+(require 'company)
+(require 'tern)
+(require 'company-tern)
+
+(add-to-list 'company-backends 'company-tern)
+(add-hook 'js2-mode-hook (lambda ()
+                           (tern-mode)
+                           (company-mode)))
+
+;; Disable completion keybindings, as we use xref-js2 instead
+(define-key tern-mode-keymap (kbd "M-.") nil)
+(define-key tern-mode-keymap (kbd "M-,") nil)
+
+;; HELM
 (require 'helm)
 (require 'helm-config)
+
+;; keybindings
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+
+(setq helm-M-x-fuzzy-match t)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
 (helm-mode 1)
 
+;; LINTING
+(require 'flycheck)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+	      (append flycheck-disabled-checkers
+		      '(javascript-jshint)))
+
+(add-hook 'js2-mode-hook
+          (defun my-js2-mode-setup ()
+            (flycheck-mode t)
+            (when (executable-find "eslint")
+              (flycheck-select-checker 'javascript-eslint))))
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; use local eslint instead of global
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+;; GIT
+(require 'magit)
 
 ;;----------------
 ;; APPEARANCE
 ;;----------------
+
 (load-theme 'gruvbox t)
 (set-face-attribute 'default nil :font "Monaco-16")
-(show-paren-mode 1)
 
 
 ;;----------------
 ;; CUSTOM VARIABLES
 ;;----------------
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
